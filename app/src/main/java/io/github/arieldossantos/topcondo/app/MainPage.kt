8c64a000983db8afb52f42c948a0b6b8c1df1d64
Ariel Reis <reis.ariel@outlook.com>
2019-04-01 00:13:39 -0300

    Criação da página principal e do listview de dados dinamicos
    Signed-off-by: Ariel Reis <reis.ariel@outlook.com>

--- app/src/main/java/io/github/arieldossantos/topcondo/app/MainPage.kt
+++ app/src/main/java/io/github/arieldossantos/topcondo/app/MainPage.kt
@@ -2,12 +2,36 @@ package io.github.arieldossantos.topcondo.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.google.firebase.firestore.FirebaseFirestore
 import io.github.arieldossantos.topcondo.R
+import kotlinx.android.synthetic.main.activity_main_page.*
 
 class MainPage : AppCompatActivity() {
+    val TAG = "MAINPAGE"
+
+    val db = FirebaseFirestore.getInstance()
+    var dados = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        db.collection("servicos")
+                .get()
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        for(document in it.result!!) {
+                            Log.d("MAIN", document.data.toString())
+                            dados.add(document.get("nome").toString())
+                        }
+
+                        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados)
+                        listaDeServicos.adapter = adapter
+                    } else {
+                        Log.w(TAG, "Erro ao obter os serviços. ", it.exception)
+                    }
+                }
     }
 }
