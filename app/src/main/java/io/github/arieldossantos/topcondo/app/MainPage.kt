03ad131e94858160dffff33d55100152aaf4bd4b
Ariel Reis <reis.ariel@outlook.com>
2019-03-31 23:11:04 -0300

    Inclusão do ListView da página principal de serviços

---
+++ app/src/main/java/io/github/arieldossantos/topcondo/app/MainPage.kt
@@ -0,0 +1,13 @@
+package io.github.arieldossantos.topcondo.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.arieldossantos.topcondo.R
+
+class MainPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
